3b72a32c815e4ebcd37e5174ba8a8ab1f3935b03
Damir Tulepov <damir.tulepov@opensoftdev.ru>
2015-02-15 14:02:20 +0300

    Simple CoreFacade

---
+++ src/org/arriva/core2048/CoreFacade.java
@@ -0,0 +1,37 @@
+package org.arriva.core2048;
+
+public class CoreFacade {
+
+    private ConjunctionRule conjunctionRule;
+    private CellRatingModifier cellRatingModifier;
+    private Field field;
+
+    public ConjunctionRule getConjunctionRule() {
+        return conjunctionRule;
+    }
+
+    public CellRatingModifier getCellRatingModifier() {
+        return cellRatingModifier;
+    }
+
+
+    public Field getField() {
+        return field;
+    }
+
+    public CoreFacade(Field field, ConjunctionRule conjunctionRule, CellRatingModifier cellRatingModifier) {
+        this.field = field;
+        this.conjunctionRule = conjunctionRule;
+        this.cellRatingModifier = cellRatingModifier;
+
+        buildLinkage();
+    }
+
+    protected void buildLinkage() {
+        cellRatingModifier.setConjunctionRule(conjunctionRule);
+        field.setConjunctionRule(conjunctionRule);
+    }
+
+
+
+}
