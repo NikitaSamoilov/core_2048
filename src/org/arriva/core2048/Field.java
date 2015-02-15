cab09620bf73791293e32b2ed98f80e5b3c3585a
Damir Tulepov <damir.tulepov@opensoftdev.ru>
2015-02-15 13:32:00 +0300

    hasEmptyCells and hasAvailableConjunction logic for Rectangle field

--- src/org/arriva/core2048/Field.java
+++ src/org/arriva/core2048/Field.java
@@ -2,6 +2,16 @@ package org.arriva.core2048;
 
 public abstract class Field {
 
+    protected ConjunctionRule conjunctionRule;
+
+    public ConjunctionRule getConjunctionRule() {
+        return conjunctionRule;
+    }
+
+    public void setConjunctionRule(ConjunctionRule conjunctionRule) {
+        this.conjunctionRule = conjunctionRule;
+    }
+
     public abstract boolean hasEmptyCells();
     public abstract boolean hasAvailableConjunction();
     public abstract int getCellCount();
--- src/org/arriva/core2048/impl/RectangleField.java
+++ src/org/arriva/core2048/impl/RectangleField.java
@@ -1,11 +1,14 @@
 package org.arriva.core2048.impl;
 
+import org.arriva.core2048.Cell;
 import org.arriva.core2048.Field;
+import org.arriva.core2048.SpecialRatingValues;
 
 public class RectangleField extends Field {
 
     private int rowCount;
     private int colCount;
+    protected Cell[][] cells;
 
     public int getRowCount() {
         return rowCount;
@@ -15,15 +18,63 @@ public class RectangleField extends Field {
         return colCount;
     }
 
+    public RectangleField(int rowCount, int colCount) {
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+        cells = new Cell[rowCount][colCount];
+    }
+
     @Override
     public boolean hasEmptyCells() {
-        // TODO: Implement logic
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                if (cells[i][j].getRating() == SpecialRatingValues.EMPTY_CELL_RATING) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public boolean hasAvailableConjunction() {
-        // TODO: Implement logic
+        if (hasAvailableHorizontalConjunction()) {
+            return true;
+        }
+
+        if (hasAvailableVerticalConjunction()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean hasAvailableVerticalConjunction() {
+        for (int i = 0; i < rowCount - 1; i++) {
+            for (int j = 0; j < colCount; j++) {
+                Cell firstCell = cells[i][j];
+                Cell secondCell = cells[i+1][j];
+
+                if (conjunctionRule.canConjuct(firstCell, secondCell)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean hasAvailableHorizontalConjunction() {
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount - 1; j++) {
+                Cell firstCell = cells[i][j];
+                Cell secondCell = cells[i][j+1];
+
+                if (conjunctionRule.canConjuct(firstCell, secondCell)) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
